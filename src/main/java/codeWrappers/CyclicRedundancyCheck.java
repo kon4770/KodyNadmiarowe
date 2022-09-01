b11eff0c43e26b9eebb48ee4e02be427413f0df8
kon4770 <56548861+kon4770@users.noreply.github.com>
2022-09-01 21:47:56 +0200

    Added CyclicRedundancyCheck and NoiceProducer

---
+++ src/main/java/codeWrappers/CyclicRedundancyCheck.java
@@ -0,0 +1,100 @@
+package codeWrappers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CyclicRedundancyCheck {
+    private String divisor = "";
+    private StringBuilder[] bitArray;
+
+    public CyclicRedundancyCheck(int divisorIndex, StringBuilder[] bitArray) {
+        Map<Integer, Integer> dividerMap = new HashMap<>();
+        dividerMap.putAll(Map.of(3, 0x5, 4, 0x9, 5, 0x12, 6, 0x23, 7, 0x44, 8, 0xD3, 10, 0x3EC, 11, 0x5C2, 12, 0xC07, 13, 0x1E7A));
+        dividerMap.putAll(Map.of(14, 0x3016, 15, 0x740A, 16, 0x8810, 17, 0x1B42D, 21, 0x18144C, 24, 0xAEB6E5, 30, 0x30185CE3, 32, 0x82608EDB));
+        if (divisorIndex > bitArray[0].length()) {
+            divisorIndex = bitArray[0].length();
+        }
+        this.divisor = String.format("%32s", Integer.toBinaryString(dividerMap.get(divisorIndex))).strip().concat("1");
+        ;
+        this.bitArray = bitArray;
+    }
+
+
+    public void encode() {
+        StringBuilder rem;
+        StringBuilder divisor = new StringBuilder(this.divisor);
+        for (int i = 0; i < bitArray.length; i++) {
+            rem = divideDataWithDivisor(bitArray[i], divisor);
+            bitArray[i].append(rem.substring(0, rem.length() - 1));
+        }
+    }
+
+    public void decode() {
+        StringBuilder divisor = new StringBuilder(this.divisor);
+        for (int i = 0; i < bitArray.length; i++) {
+            if(!isDataCorrect(bitArray[i], divisor)){
+                System.out.println("Mistake in " + i);
+            }
+        }
+        System.out.println("Decoding done!");
+    }
+
+    private StringBuilder divideDataWithDivisor(StringBuilder oldData, StringBuilder divisor) {
+//        System.out.println("oldData " + oldData);
+//        System.out.println("divisor " + divisor);
+
+        StringBuilder data = new StringBuilder("0".repeat(oldData.length() + divisor.length()));
+        StringBuilder rem = new StringBuilder("0".repeat(divisor.length()));
+
+        data.replace(0, oldData.length(), oldData.toString());
+        rem.replace(0, divisor.length(), data.substring(0, divisor.length()));
+
+//        System.out.println(data);
+//        System.out.println(rem);
+        for (int i = 0; i < oldData.length(); i++) {
+//            System.out.println((i + 1) + ".) First data bit is : " + rem.charAt(0));
+//            System.out.print("Remainder : ");
+            if (rem.charAt(0) == '1') {
+                for (int j = 1; j < divisor.length(); j++) {
+                    rem.setCharAt(j - 1, exorOperation(rem.charAt(j), divisor.charAt(j)));
+//                    System.out.print(rem.charAt(j - 1));
+                }
+            } else {
+                for (int j = 1; j < divisor.length(); j++) {
+                    rem.setCharAt(j - 1, exorOperation(rem.charAt(j), '0'));
+//                    System.out.print(rem.charAt(j - 1));
+                }
+            }
+            try {
+                rem.replace(divisor.length() - 1, divisor.length(), String.valueOf(data.charAt(i + divisor.length())));
+            } catch (Exception e) {
+//                System.out.println("asa " + (divisor.length() - 1) + " " + (i + divisor.length()));
+//                System.out.println("Aaa");
+            }
+//            System.out.println(rem.charAt(divisor.length() - 1));
+        }
+//        System.out.println("rem " + rem);
+        return rem;
+    }
+
+    private char exorOperation(char x, char y) {
+        if (x == y) {
+            return '0';
+        }
+        return '1';
+    }
+
+    private boolean isDataCorrect(StringBuilder data, StringBuilder divisor) {
+
+        StringBuilder rem = divideDataWithDivisor(data, divisor);
+//        System.out.println("Koncowy " + rem);
+        for (int i = 0; i < rem.length(); i++) {
+            if (rem.charAt(i) != '0') {
+                System.out.println("Currupted data received...");
+                return false;
+            }
+        }
+//        System.out.println("Data received without any error.");
+        return true;
+    }
+}  
--- src/main/java/logic/Logic.java
+++ src/main/java/logic/Logic.java
@@ -1,13 +1,15 @@
 package logic;
 
 import codeAlters.BufferAndChunkSetter;
+import codeAlters.NoiseProducer;
+import codeWrappers.CyclicRedundancyCheck;
 import codeWrappers.TwoDimParityCheck;
 import codeWrappers.WrapperInterface;
 
 public class Logic {
-    public void run(){
+    public void run() {
         PNGReader reader = new PNGReader();
-        int[][] before = reader.readImageToIntegerArray("DangerNoodle.png");
+        int[][] before = reader.readImageToIntegerArray("Fale.png");
         int height = reader.getHeight();
         int width = reader.getWidth();
         char[] bitArray = new char[width * height * 32];
@@ -22,20 +24,20 @@ public class Logic {
             }
         }
 
-        BufferAndChunkSetter bufferAndChunk = new BufferAndChunkSetter(64, 0);
+        BufferAndChunkSetter bufferAndChunk = new BufferAndChunkSetter(17, 0);
         StringBuilder[] bitMatrix = bufferAndChunk.divideString(String.valueOf(bitArray));
 
-//        WrapperInterface simpleParityCheck = new SimpleParityCheck(bitMatrix);
-        WrapperInterface simpleParityCheck = new TwoDimParityCheck(bitMatrix,4);
-        simpleParityCheck.encode();
-        System.out.println("Encoding Done");
-        char replaceWith = bitMatrix[0].charAt(3);
-        bitMatrix[0].replace(3, 4, replaceWith == '0' ? "1" : "0");
-        replaceWith = bitMatrix[8].charAt(3);
-        bitMatrix[8].replace(3, 4, replaceWith == '0' ? "1" : "0");
-        simpleParityCheck.decode(((TwoDimParityCheck) simpleParityCheck).getBitArrayCopy());
-        System.out.println(simpleParityCheck.hasChanged());
-        System.out.println(simpleParityCheck.getChunkToResendSet());
+
+        CyclicRedundancyCheck cyclicRedundancyCheck = new CyclicRedundancyCheck(5,bitMatrix);
+        cyclicRedundancyCheck.encode();
+
+        NoiseProducer noiseProducer = new NoiseProducer(1);
+        bitMatrix = noiseProducer.introduceNoise(bitMatrix);
+        System.out.println("Bledy sa w " + noiseProducer.getContaminatedChunkSet());
+        System.out.println(noiseProducer.getContaminatedChunkSet().size());
+
+        cyclicRedundancyCheck.decode();
+
 
         /*
         tutaj bedzie cala logika
@@ -44,7 +46,7 @@ public class Logic {
         index = 0;
         int[] integerResultArray = new int[width * height];
 //        String bitString = new String(bitArray);
-        String bitString = new String(bufferAndChunk.extractEsenseAndMergeString(bitMatrix));
+        String bitString = new String(bufferAndChunk.extractEsseneAndMergeString(bitMatrix));
         for (int i = 0; i < height * width; i++) {
             String bitSubString = bitString.substring(i * 32, i * 32 + 32);
             int bitRepresentationStart = bitSubString.indexOf('1');
