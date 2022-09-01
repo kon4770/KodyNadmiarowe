12186b62b089417a6a91afce18796b9cdbb3812c
kon4770 <56548861+kon4770@users.noreply.github.com>
2022-09-01 21:49:24 +0200

    Added CyclicRedundancyCheck and NoiceProducer

---
+++ src/main/java/codeAlters/NoiseProducer.java
@@ -0,0 +1,44 @@
+package codeAlters;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class NoiseProducer {
+    private int noiseDensityPerMillionBits;
+    private Set<Integer> contaminatedChunkSet = new HashSet<>();
+
+    public NoiseProducer(int noiseDensityPerMillionBits) {
+        this.noiseDensityPerMillionBits = noiseDensityPerMillionBits;
+    }
+
+    public StringBuilder[] introduceNoise(StringBuilder[] cleanData) {
+        StringBuilder[] contaminatedData = cleanData;
+        int chunkSize = cleanData[0].length();
+        int chunkNumber = cleanData.length;
+
+        int bitsToContaminate = chunkSize * chunkNumber * noiseDensityPerMillionBits / 1000000;
+        System.out.println(chunkNumber * chunkSize);
+        System.out.println(bitsToContaminate);
+        long maxBitIndex = chunkSize * chunkNumber;
+        for (int i = 0; i < bitsToContaminate; i++) {
+            long randomBit = ThreadLocalRandom.current().nextLong(maxBitIndex);
+            int chunkId = (int) randomBit / chunkSize;
+            int bitIdInsideChunk = (int) randomBit % chunkSize;
+            contaminatedData[chunkId] = flipBit(contaminatedData[0],bitIdInsideChunk);
+            contaminatedChunkSet.add(chunkId);
+        }
+
+        return contaminatedData;
+    }
+
+    private StringBuilder flipBit(StringBuilder cleanArray, int bitIndex){
+        String replaceWith = cleanArray.charAt(bitIndex)=='0'?"1":"0";
+        StringBuilder contaminatedArray = cleanArray.replace(bitIndex,bitIndex+1,replaceWith);
+        return contaminatedArray;
+    }
+
+    public Set<Integer> getContaminatedChunkSet(){
+        return contaminatedChunkSet;
+    }
+}
